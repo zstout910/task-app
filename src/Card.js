5477b66f3aa4e96580b4f5e35e0e9e78c21bde67
Zach Stout <zistout910@gmail.com>
2024-08-02 00:58:31 -0400

    added card delete icon

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Card.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function Card({ title, items, handleDelete }) {
   return (
     <div className="card">
-      <h3>{title}</h3>
+      
+      <h3 className='title'>{title}</h3>
       <ul>
         {items.map((item, idx) => (
-          <li className="card-item" key={idx}><input type="checkbox"/> {item} <button className="edit-btn"><FontAwesomeIcon icon={faPenToSquare} /></button></li>
+          <li className="card-item" key={idx}><input type="checkbox"/> {item} </li>
         ))}
       </ul>
+      <div className="card-bottom">
+      <button className="edit-btn"><FontAwesomeIcon icon={ faTrash } /></button>
+        </div>
     </div>
   );
 }
